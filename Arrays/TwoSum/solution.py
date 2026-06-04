600e1a0463f3c8734b35aa34de6502bf649acd12
Shruti Sharma <97786751+shruti244001@users.noreply.github.com>
2026-06-05 03:33:18 +0530

    add optimal hashmap solution for Two Sum
    Implement twoSum method to find indices of two numbers that add up to target.

--- Arrays/TwoSum/solution.py
+++ Arrays/TwoSum/solution.py
@@ -1,1 +1,11 @@
+class Solution:
+    def twoSum(self, nums, target):
+        hashmap = {}
 
+        for i in range(len(nums)):
+            rem = target - nums[i]
+
+            if rem in hashmap:
+                return [hashmap[rem], i]
+
+            hashmap[nums[i]] = i
